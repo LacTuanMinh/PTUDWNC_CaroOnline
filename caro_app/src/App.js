78f21ada557876d24d10741c0a9df6e95f2ba8be
Khanh Nguyen Ho <hokhanhnguyen23@gmail.com>
2020-12-05 15:03:59 +0700

    Add components
    - navbar, footer
    - login, signup, profile, home, games
    - chat ui
    - basic gameplay

--- caro_app/src/App.js
+++ caro_app/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Game from './components/Game/game';
+import Home from './components/Home/home';
+import Login from './components/Login/login';
+import Profile from './components/Profile/profile';
+import SignUp from './components/SignUp/signup';
+import Navbar from './components/Navbar/navbar';
+import Footer from './components/Footer/footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br></br>
+      <div className="App">
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/profile'>
+            <Profile />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/games'>
+            <Game />
+          </Route>
+          <Route path='/signup'>
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+      <br></br>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ caro_app/src/components/Game/board.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Square from './square'
+import config from '../../constants/config.json';
+
+function Board({squares, winLine, onClick}) {
+  const boardSize = config.boardSize;
+  const renderSquare = (i) => {  
+    return (
+      <Square
+        key={i}
+        value={squares[i]}
+        isHighlight={winLine && winLine.includes(i)}
+        onClick={() => onClick(i)}
+      />
+    );
+  };
+
+  const createSquares = () => {
+    let squares = [];
+    for (let i = 0; i < boardSize; i++) {
+      let rows = [];
+      for (let j = 0; j < boardSize; j++) {
+        rows.push(renderSquare(boardSize * i + j));
+      }
+      squares.push(
+        <div className="board-row">
+          {rows}
+        </div>
+      );
+    }
+    return squares;
+  };
+
+  return (
+    <div>
+      {createSquares()}
+    </div>
+  );
+}
+
+export default Board;
