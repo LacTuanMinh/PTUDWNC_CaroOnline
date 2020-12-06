0341efa7214fe66a19d8071fa24f68ad2333a41a
Khanh Nguyen Ho <hokhanhnguyen23@gmail.com>
2020-12-06 14:29:56 +0700

    Update
    nothing important

--- caro_app/src/components/OnlineUsers/index.js
+++ caro_app/src/components/OnlineUsers/index.js
@@ -18,131 +18,123 @@ import FaceIcon from '@material-ui/icons/Face';
 const drawerWidth = 300;
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginRight: drawerWidth,
-    },
-    title: {
-        flexGrow: 1,
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-start',
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginRight: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginRight: 0,
-    },
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  },
 }));
 
 export default function OnlineUsers() {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
-    const userList = ["Nguyen Van A", "Đỗ Thị Mai Chờ", "Phan La Nuong"];
+  const userList = ["Nguyen Van A", "Đỗ Thị Mai Chờ", "Phan La Nuong"];
 
-    return (
-        <div className={classes.root}>
-            <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="end"
-                onClick={handleDrawerOpen}
-                className={clsx(open && classes.hide)}
-                style={{
-                    position: 'absolute',
-                    top: -10,
-                    right: 50,
-                    zIndex: 1,
-                    alignContent: 'center',
-                    fontSize: '4',
-                    borderRadius: '50%',
-                    height: '60px',
-                    width: '60px',
-                    color: '#FFF',
-                    backgroundImage: 'linear-gradient(to right, #24C6DC 0%, #514A9D 100%)',
-                    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
-                }}
-            >
-                <GroupAddIcon />
-            </IconButton>
-            <Drawer
-                className={classes.drawer}
-                variant="temporary"
-                anchor="right"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-                onClose={handleDrawerClose}
-                
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        <CloseIcon />
-                    </IconButton>
-                    <Input placeholder="Username here" />
-                </div>
-                <Divider />
-                <List>
-                    {userList.map((item) => (
-                        <ListItem key={item}>
-                            <ListItemIcon>
-                                <FaceIcon />
-                            </ListItemIcon>
-                            <ListItemText primary={item} style={{ fontSize: 'small' }} />
-                            <Button variant="outlined" style={{ fontSize: '7px', borderRadius: '5px' }}>Invite</Button>
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
+  return (
+    <div className={classes.root}>
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        edge="end"
+        onClick={handleDrawerOpen}
+        className={clsx(open && classes.hide)}
+        style={{
+          position: 'absolute',
+          top: -10,
+          right: 50,
+          zIndex: 1,
+          alignContent: 'center',
+          fontSize: '4',
+          borderRadius: '50%',
+          height: '60px',
+          width: '60px',
+          color: '#FFF',
+          backgroundImage: 'linear-gradient(to right, #24C6DC 0%, #514A9D 100%)',
+          boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+        }}
+      >
+        <GroupAddIcon />
+      </IconButton>
+      <Drawer className={classes.drawer} variant="temporary" anchor="right" open={open}
+        classes={{ paper: classes.drawerPaper }} onClose={handleDrawerClose}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <CloseIcon />
+          </IconButton>
+          <Input placeholder="Username here" />
         </div>
-    );
+        <Divider />
+        <List>
+          {userList.map((item) => (
+            <ListItem key={item}>
+              <ListItemIcon>
+                <FaceIcon />
+              </ListItemIcon>
+              <ListItemText primary={item} style={{ fontSize: 'small' }} />
+              <Button variant="outlined" style={{ fontSize: '7px', borderRadius: '5px' }}>Invite</Button>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
 }
